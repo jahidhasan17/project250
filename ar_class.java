bbb3efacb76dd8337010641266928f2e2378bc37
jahidhasan17 <41954263+jahidhasan17@users.noreply.github.com>
2020-01-21 17:52:26 +0600

    Add files via upload

---
+++ ar_class.java
@@ -0,0 +1,14 @@
+package com.example.home_activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ar_class extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ar_class);
+    }
+}
